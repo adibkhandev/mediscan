41fe6073c0a029159a03e8adf2e8fccbdef5fdbe
adibkhandev <adibkhaan001@gmail.com>
2025-05-02 15:28:00 +0600

    test

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Ham from './../assets/ham.svg'
+import TextLogo from './../assets/text-logo.svg'
+import Write from './../assets/write.svg'
+import Logo from './../assets/logo.svg'
+import Image from './../assets/logo.svg'
+import Speaker from './../assets/speaker.svg'
+
+export const Home = () => {
+    return(
+       <div className="home-container">
+          <div className="nav-cont">
+            <img src={Ham} alt="" />
+            <img src={TextLogo} alt="" />
+            <img src={Write} alt="" />
+          </div>
+          <div className="intro-cont">
+            <img src={Logo} alt="" />
+            <div className="heading">
+              Upload your infection picture and i will help curing
+            </div>
+          </div>
+          <div className="input-cont"></div>
+       </div>
+    )
+}
